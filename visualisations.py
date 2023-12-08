a003ef538e1a1f499db3ddd3fe6e53a992d2b77b
ElizabethisBest <tianyuhu@umich.edu>
2023-12-07 23:25:03 -0500

    temp&flu

--- visualisations.py
+++ visualisations.py
@@ -1,3 +1,6 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def draw_flu_and_population_bar():
@@ -6,8 +9,56 @@ def draw_flu_and_population_bar():
 def draw_quarterly_flu_bar():
     pass
 
+db_connection = sqlite3.connect('database.db')
+
 def draw_temp_flu_scatter():
-    pass
+
+    conn = sqlite3.connect('database.db')
+
+    flu_data_query = "SELECT * FROM flu_data"
+    flu_data = pd.read_sql_query(flu_data_query, conn)
+
+    temp_data_query = "SELECT * FROM quarterly_temp"
+    temp_data = pd.read_sql_query(temp_data_query, conn)
+
+    conn.close()
+
+    merged_data = pd.merge(flu_data, temp_data, left_on='state', right_on='state_abbr')
+
+    merged_data = merged_data[merged_data[['q1_temp', 'q2_temp', 'q3_temp', 'q4_temp']].mean(axis=1) != 0]
+
+    average_temp = merged_data[['q1_temp', 'q2_temp', 'q3_temp', 'q4_temp']].mean()
+    average_flu_patients = merged_data.groupby('quarter')['num_patients'].mean()
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:blue'
+    ax1.set_xlabel('Quarter')
+    ax1.set_ylabel('Average Temperature', color=color)
+    bars = ax1.bar(average_temp.index, average_temp, color=color, label='Average Temperature')
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    for bar, label in zip(bars, average_temp):
+        height = bar.get_height()
+        ax1.text(bar.get_x() + bar.get_width() / 2, height + 0.1, f'{label:.2f}', ha='center', va='bottom', color='black')
+
+    ax2 = ax1.twinx()  
+    color = 'yellow'  
+    ax2.set_ylabel('Average Flu Patients', color=color)  
+    line = ax2.plot(average_flu_patients.index, average_flu_patients, color=color, marker='o', label='Average Flu Patients')
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    for i, txt in enumerate(average_flu_patients):
+        ax2.annotate(f'{txt:.2f}', (average_flu_patients.index[i], txt + 10000), color='black', ha='center', va='bottom')
+
+    lines, labels = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines + lines2, labels + labels2, loc='upper left', bbox_to_anchor=(0.7, 1.0))
+
+    plt.title('Comparison between Average Temperature and Average Flu Patient Number')
+    plt.show()
+
+
 
 if __name__ == '__main__':
     draw_flu_and_population_bar()
