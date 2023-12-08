647eb61be20ffc190fc62bc21c9b038b8a596edf
Mark Chen <yanlic6@uci.edu>
2023-12-07 21:23:19 -0500

    visulization

---
+++ visualisations.py
@@ -0,0 +1,15 @@
+
+
+def draw_flu_and_population_bar():
+    pass
+
+def draw_quarterly_flu_bar():
+    pass
+
+def draw_temp_flu_scatter():
+    pass
+
+if __name__ == '__main__':
+    draw_flu_and_population_bar()
+    draw_quarterly_flu_bar()
+    draw_temp_flu_scatter()
